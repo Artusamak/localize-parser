3463ea4f9873cdfbbed60b0a60415efdfe75735f
Artusamak <julien@commerceguys.com>
2013-07-29 18:00:05 +0200

    Implements version identification of a given project.

--- src/LdoParser/LocalizeParser.php
+++ src/LdoParser/LocalizeParser.php
@@ -10,57 +10,123 @@ namespace LdoParser;
 
 class LocalizeParser {
   var $base_url = 'http://ftp.drupal.org/files/translations/7.x/';
+  var $update_url = 'http://updates.drupal.org/release-history/';
   var $language = 'fr';
+  var $major_version = '7.x';
   var $version = '7.x-1.0';
   var $projects = array();
   var $output = '';
 
   var $tmp_counter = 0;
 
+  /**
+   * Getter to fetch the generated output.
+   */
   function getOutput() {
     return $this->output;
   }
 
+  /**
+   * Builds the projects list and download the projects po files.
+   */
   function buildProjects() {
+    // Parse the html dump of the ftp page listing the projects.
+    // @see dump at $base_url.
     $this->prepareProjectsList();
-    // Iterate on the projects.
-    $this->downloadPojectsFiles();
+
+    // Download the projects translations.
+    $this->downloadProjectsFiles();
   }
 
-  function downloadPojectsFiles() {
-    while(list( , $project) = each($this->projects)) {
+  /**
+   * Download the projects translations.
+   *
+   * Given a set of projects, fetch the appropriate .po files.
+   */
+  function downloadProjectsFiles() {
+    while(list( , $project) = each($this->projects_raw)) {
       if ($this->tmp_counter < 3) {
-        $this->buildProjectDetails($project);
+        $clean_project_name = substr($project, 0, -1);
+        $this->buildProjectDetails($clean_project_name);
       }
       $this->tmp_counter++;
     }
   }
 
+  /**
+   * Builds the report of a module.
+   *
+   * Stores the report output of a given module.
+   *
+   * @param $project
+   *   Project machine name.
+   */
   function buildProjectDetails($project) {
-    $clean_project_name = substr($project, 0, -1);
-    $translation_file = $this->base_url . $project . $clean_project_name . '-' . $this->version . '.' . $this->language . '.po';
+    // Prepare the download url of a project.
+    $version = $this->fetchProjectVersion($project);
 
-    $headers = get_headers($translation_file);
-    // e.g: HTTP/1.1 404 Not Found
+
+    // Prepare the transaction URL.
+    // Extracts the return code from headers to check if the file exists.
+    // It can be 200 or 404 e.g: HTTP/1.1 404 Not Found
+    $translation_url = $this->base_url . $project . '/' . $project . '-' . $version . '.' . $this->language . '.po';
+    $headers = get_headers($translation_url);
     $code = array_slice(explode(' ', array_shift($headers)), 1, -1);
 
-    // Check if the file exists, it means that we expect a 200 code as answer.
+    // Only look for 200 code as answer.
     if ($code[0] == '200') {
-      $translation_content = file_get_contents($translation_file);
-//      $filename = __DIR__ . '/../downloads/' . $clean_project_name . '/' . $clean_project_name . '-' . $this->version . '.' . $this->language . '.po';
-      $filename = __DIR__ . '/../downloads/' . $clean_project_name . '-' . $this->version . 'po';
-      $f = fopen($filename, 'w+');
+      // Download the file in our downloads folder.
+      // @TODO decide what to do of processed files.
+      $translation_content = file_get_contents($translation_url);
+      $translation_file = __DIR__ . '/../../downloads/' . $project . '-' . $version . '.po';
+      $f = fopen($translation_file, 'w+');
       if ($f) {
         fwrite($f, $translation_content);
         fclose($f);
 
-        $this->output .= $translation_file . '<br />';
-        $this->output .= '<p>Module name: ' . $project . "<br />\n";
-        $this->output .= '</p>';
+        // @TODO replace with partner's code.
+        $this->mimicReport($project);
       }
     }
   }
 
+  /**
+   * Fetchs the latest version number of a given project.
+   *
+   * @param $project_name
+   *   Machine name of a project.
+   *
+   * @return mixed
+   *   Returns the string of the project's version.
+   */
+  function fetchProjectVersion($project_name) {
+    $update_url = $this->update_url . $project_name . '/' . $this->major_version;
+    $xml = simplexml_load_file($update_url);
+
+    $version = $xml->releases->release[0]->version;
+    $this->projects[$project_name]['version'] = $version;
+
+    return $version;
+  }
+
+  /**
+   * Blabla to describe the report.
+   * It won't survive anyway.
+   *
+   * @param $project
+   */
+  function mimicReport($project) {
+    $this->output .= '<p>Project: ' . $project . "<br />\n";
+    $this->output .= 'Version: ' . $this->projects[$project]['version'] . '<br />';
+    $this->output .= '[insert stats] <br />';
+    $this->output .= '</p>';
+  }
+
+  /**
+   * Parses the html file listing projects.
+   *
+   * Stores an array of the projects.
+   */
   function prepareProjectsList() {
     // Get file content.
     $filename = __DIR__ . '/../../snippet.modules.list.html';
@@ -74,6 +140,6 @@ class LocalizeParser {
     $doc->strictErrorChecking = FALSE;
     $doc->loadHTML($contents);
     $xml = simplexml_import_dom($doc);
-    $this->projects = $xml->xpath('body/div[2]/pre/a[position()>5]');
+    $this->projects_raw = $xml->xpath('body/div[2]/pre/a[position()>5]');
   }
 }
